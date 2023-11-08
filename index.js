28f482fb69d477d435228477166229848d1c9724
Mirza Sahil Alli Baig <mirzasahilallibaig@gmail.com>
2023-11-09 04:57:01 +0530

    modified

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+app.use("/",express.static(path.join(__dirname,"Static")));
 // File storage
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -57,3 +58,6 @@ mongoose
     // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
+
+
+
