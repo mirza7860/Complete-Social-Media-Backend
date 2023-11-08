cda63849cf1c9586619868b01b5de30733bb22e2
Mirza Sahil Alli Baig <136656845+mirza7860@users.noreply.github.com>
2023-11-09 05:08:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
-app.use("/",express.static(path.join(__dirname,"Static")));
+app.use("/",express.static(path.join(__dirname,"static")));
 // File storage
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
