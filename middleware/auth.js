d4e64e568ff04f2af0b05a5a1e42ba09607a784a
Mirza Sahil Alli Baig <mirzasahilallibaig@gmail.com>
2023-11-08 12:52:47 +0530

    updated

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ export const verifyToken = async (req, res, next) => {
     let token = req.header("Authorization");
 
     if (!token) {
-      return res.staus(403).send("Access Denied");
+      return res.status(403).send("Access Denied");
     }
     if (token.startsWith("Bearer ")) {
       token = token.slice(7, token.length).trimLeft();
@@ -14,7 +14,7 @@ export const verifyToken = async (req, res, next) => {
     req.user = verified;
     next();
   } catch (error) {
-    res.status(500).json({ error: err.message });
+    res.status(500).json({ error: error.message });
   }
 };
 
